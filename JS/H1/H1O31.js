8dd78379a99aaecd4f3d16b4e6647d060a9bdbbd
sennederent <sennedrent@gmail.com>
2020-03-02 10:53:23 +0000

    piramier

--- JS/H1/H1O31.js
+++ JS/H1/H1O31.js
@@ -1,16 +1,22 @@
 kleur='white';
 
 function setup() {
-  var myCanvas = createCanvas(451,451);
+  var myCanvas = createCanvas(501,501);
   myCanvas.parent('processing');
   background('coral');
 }
 
 function draw() {
-  for (var rij = 0;rij < 450;rij += 50) {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
-      fill(kleur);
-      rect(kolom,rij,50,50);
+  for (var rij = 0;rij < 500;rij += 50) {
+    for (var kolom = 0;kolom < 500;kolom += 50) {
+      if ((rij % 100 == 0 && kolom % 100 != 0) || (kolom % 100 == 0 && rij % 100 != 0)) {
+        fill(kleur);
+        rect(kolom,rij,50,50);
+      }
+      else {
+        fill('black');
+        rect(kolom,rij,50,50);
+      }
     }
   }
 }
--- JS/H1/H1O34.js
+++ JS/H1/H1O34.js
@@ -7,12 +7,17 @@ function setup() {
   var myCanvas = createCanvas(451,451);
   myCanvas.parent('processing');
   frameRate(5);
-  noLoop();
+  // noLoop();
   strokeWeight(4);
   stroke('steelblue');
 }
 
 function draw() {
   background('white');
-
+  for (var c = 0; c <= 100; c++) {
+    x = random(50, 400);
+    y = random(50, 400);
+    diameter = random(25, 75);
+    ellipse(x, y, diameter);
+  }
 }
